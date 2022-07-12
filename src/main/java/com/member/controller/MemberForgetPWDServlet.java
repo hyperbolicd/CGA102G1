239bd8b12928dc3d888e963602cfa8c0b30123d8
Stella <wanlly1993@gmail.com>
2022-07-12 14:21:46 +0800

    忘記密碼(錯誤處理) ﹑登入驗證錯誤處理

--- src/main/java/com/member/controller/MemberForgetPWDServlet.java
+++ src/main/java/com/member/controller/MemberForgetPWDServlet.java
@@ -3,8 +3,10 @@ package com.member.controller;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -21,40 +23,57 @@ import com.member.model.MemberService;
 @WebServlet("/member/password/forget")
 public class MemberForgetPWDServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	public static final Gson gson = new GsonBuilder().create(); 
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public MemberForgetPWDServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	public static final Gson gson = new GsonBuilder().create();
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public MemberForgetPWDServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 //		response.getWriter().append("Served at: ").append(request.getContextPath());
 		request.setCharacterEncoding("UTF-8");
 		response.setCharacterEncoding("UTF-8");
+		PrintWriter pw = response.getWriter();
+		Map<String, String> map = new HashMap<>();
+
+		// 獲取前端傳過來訊息
+		String usrEmail = null;
+		Integer verifyResult = null;
 		
-		//獲取前端傳過來訊息
-		String usrEmail = request.getParameter("usrEmail");
-		Map<String, String> map= new HashMap<>();
-		map.put("stat", "success");
+		if (!request.getParameter("usrEmail").isEmpty()) {
+			usrEmail = request.getParameter("usrEmail");
+			MemberService memberService = new MemberService();
+			verifyResult = memberService.verifyEmail(usrEmail);
+		} 
+		if (verifyResult == 0) {
+			map.put("stat", "fail");
+			pw.print(gson.toJson(map));
+			return;
+		}
+		
+		
 //		System.out.println("123");
 		MemberService memberservice = new MemberService();
-		memberservice.sendMail(usrEmail);
-		try(PrintWriter pw = response.getWriter()){
-			pw.print(gson.toJson(map));
-		} catch(Exception e){
-			e.printStackTrace();
-			
+		boolean res = memberservice.sendMail(usrEmail);
+		
+		if (res) {
+			map.put("stat", "success");
+		} else {
+			map.put("stat", "fail");
 		}
-	}
+		
+		pw.print(gson.toJson(map));
 
-	
+	}
 
 }
